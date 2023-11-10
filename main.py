c326cc9692d7f6da298ce8e620f74c58d4f6e430
kubajznicek <kubajznicek0@gmail.com>
2023-11-10 21:26:46 +0100

    functions for reading chnnels from analog multiplexer

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-# blink example
-
 import time
 import board # type: ignore
 import neopixel # type: ignore
+import analogio # type: ignore
+
+from functions import SetChannel, SetUpPinsForMCP
+
 
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
-print("hello kuba")
+analog_pin = analogio.AnalogIn(board.A0)
+
+mcp_pins = SetUpPinsForMCP((board.D0, board.D1, board.D2, board.D3))
+
+print(mcp_pins)
+
+
+
+    
+SetChannel(0, mcp_pins)
 
 while True:
-    print("hello kuba")
-    pixels.fill((255, 0, 0))
-    time.sleep(0.5)
-    pixels.fill((0, 0, 255))
-    time.sleep(0.5)
+    print("cisty signal", analog_pin.value)
+    print()
+    time.sleep(0.1)
