90983f20e5b38a35bcd8d0bca20a610220671597
kubajznicek <kubajznicek0@gmail.com>
2023-11-28 18:28:20 +0100

    renaming files, autokey testing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
 import board # type: ignore
 import keypad  # type: ignore
-import digitalio # type: ignore
 import busio # type: ignore
 import array
 from micropython import const # type: ignore
@@ -13,8 +11,8 @@ from adafruit_hid.consumer_control import ConsumerControl # type: ignore
 from adafruit_hid.keyboard import Keyboard  # type: ignore
 
 from functions import log_cpu_info
-from matrix_config import MATRIX_ACTIONS
-from AnalogSignalProcessor import AnalogSignalProcessor
+from config import MATRIX_ACTIONS, ANALOG_ACTIONS
+from analog_signal_processor import AnalogSignalProcessor
 
 
 
@@ -29,8 +27,10 @@ matrix = keypad.KeyMatrix([board.D5, board.D6], [board.D7, board.D8])
 cc = ConsumerControl(usb_hid.devices)
 kbd = Keyboard(usb_hid.devices)
 
+
 analog_values = array.array("H", [0]*16)
 ANALOG_THRESHOLD:int = const(300)
+
 
 
 
@@ -45,7 +45,6 @@ for channel in range(0, 16):
 log_cpu_info()
 print("Ready!")
 
-
 while True:
     key_event = matrix.events.get()
 
@@ -56,6 +55,7 @@ while True:
 
     #region Analog Read
 
+    # NOTE with triggered count
     # for channel in range(0, 16):
     #     my_analog.set_channel(channel)
     #     difference = abs(analog_values[channel] - my_analog.read_analog())
@@ -64,10 +64,7 @@ while True:
     #         increased = analog_values[channel] < my_analog.read_analog()
     #         # print("channel", channel, "difference", difference, "triggered", triggered)
     #         for i in range(0, triggered):
-    #             if increased:
-    #                 cc.send(ConsumerControlCode.VOLUME_INCREMENT)
-    #             else:
-    #                 cc.send(ConsumerControlCode.VOLUME_DECREMENT)
+    #             ANALOG_ACTIONS[channel][increased](cc)
 
 
     for channel in range(0, 16):
@@ -75,42 +72,8 @@ while True:
         difference = abs(analog_values[channel] - my_analog.read_analog())
         if difference > ANALOG_THRESHOLD:
             print("channel", channel, "difference", difference)
-
-        
-
-            # increased = analog_values[channel] < my_analog.read_analog()
-            # # print("channel", channel, "difference", difference, "triggered", triggered)
-            # if increased:
-            #     cc.send(ConsumerControlCode.VOLUME_INCREMENT)
-            # else:
-            #     cc.send(ConsumerControlCode.VOLUME_DECREMENT)
-
-
-
+            increased = analog_values[channel] < my_analog.read_analog()
+            ANALOG_ACTIONS[channel][increased](cc)
 
         analog_values[channel] = my_analog.read_analog()
-
-
-
-
-
-
-
-
-    # start_time = time.time()
-    # for i in range(0, 1000):
-        # for channel in range(0, 16):
-            # my_analog.set_channel(channel)
-            # my_analog.read_analog()
-    # end_time = time.time()
-    # print(f"The code took {end_time - start_time} seconds to run.")
-
-
-
-
-    # print("cisty signal", my_analog.read_analog())
-
     #endregion
-
-    
-    # time.sleep(0.06)
