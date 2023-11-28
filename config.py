301d9cbf33693b50e705da7d2f6c05b553d68023
kubajznicek <kubajznicek0@gmail.com>
2023-11-28 18:28:20 +0100

    renaming files, autokey testing

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
 from adafruit_hid.consumer_control_code import ConsumerControlCode # type: ignore
-from adafruit_hid.keycode import Keycode
+from adafruit_hid.keycode import Keycode # type: ignore
 
 MATRIX_ACTIONS = {
     0: lambda cc, kbd: cc.send(ConsumerControlCode.PLAY_PAUSE),
     1: lambda cc, kbd: cc.send(ConsumerControlCode.MUTE),
     2: lambda cc, kbd: cc.send(ConsumerControlCode.SCAN_NEXT_TRACK),
     # 3: lambda cc: cc.send(ConsumerControlCode.SCAN_PREVIOUS_TRACK),
-    3: lambda cc, kbd: kbd.send(Keycode.C),
+    3: lambda cc, kbd: kbd.send(Keycode.CONTROL, Keycode.ALT, Keycode.SHIFT, Keycode.PAGE_DOWN),
+}
+
+ANALOG_ACTIONS = {
+    10: {
+        True: lambda cc: cc.send(ConsumerControlCode.VOLUME_INCREMENT),
+        False: lambda cc: cc.send(ConsumerControlCode.VOLUME_DECREMENT),
+    },
 }
