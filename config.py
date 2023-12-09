092e613bc0a1889ffe00daa667e4c9984130d240
kubajznicek <kubajznicek0@gmail.com>
2023-12-09 20:08:45 +0100

    light toggle automation

--- config.py
+++ config.py
@@ -5,8 +5,7 @@ from micropython import const # type: ignore
 MATRIX_ACTIONS = {
     0: lambda cc, kbd: cc.send(ConsumerControlCode.PLAY_PAUSE),
     1: lambda cc, kbd: cc.send(ConsumerControlCode.MUTE),
-    2: lambda cc, kbd: cc.send(ConsumerControlCode.SCAN_NEXT_TRACK),
-    # 3: lambda cc: cc.send(ConsumerControlCode.SCAN_PREVIOUS_TRACK),
+    2: lambda cc, kbd: kbd.send(Keycode.CONTROL, Keycode.ALT, Keycode.SHIFT, Keycode.PAGE_UP),
     3: lambda cc, kbd: kbd.send(Keycode.CONTROL, Keycode.ALT, Keycode.SHIFT, Keycode.PAGE_DOWN),
 }
 
@@ -18,4 +17,4 @@ ANALOG_ACTIONS = {
 }
 
 ANALOG_THRESHOLD:int = const(300) # optimal value for Windows 10 is around 200
-DEBUG:bool = const(True)
+DEBUG:bool = const(False)
