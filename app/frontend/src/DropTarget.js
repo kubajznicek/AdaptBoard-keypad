cc62d3e6fbf34e71806a0722cdc34ab5844eace9
kubajznicek <kubajznicek0@gmail.com>
2023-12-16 09:59:24 +0100

    add image to DnD

--- app/frontend/src/DropTarget.js
+++ app/frontend/src/DropTarget.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import "./styles/DropTarget.css";
+import DraggableItem from "./DraggableItem";
 
 class DropTarget extends React.Component {
     constructor(props) {
@@ -25,7 +26,9 @@ class DropTarget extends React.Component {
     render() {
         return (
             <div className="drop-area" onDragOver={this.handleDragOver} onDrop={this.handleDrop}>
-                {this.state.droppedItem && <div>{this.state.droppedItem.content}</div>}
+                {/* {this.state.droppedItem && <div>{this.state.droppedItem.content}</div>} */}
+                {this.state.droppedItem && <DraggableItem image={this.state.droppedItem.image}>{this.state.droppedItem.content}</DraggableItem>}
+
             </div>
         );
     }
