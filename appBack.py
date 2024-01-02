6e8f061965a4f74e5d9f8dc173f741a934f082ab
Loulky <lukas.pop005@gmail.com>
2024-01-02 16:27:50 +0100

    new way of generating app.py file; generation more automated with input array; generating matrix and analog actions

--- appBack.py
+++ appBack.py
@@ -2,40 +2,64 @@ import autopep8
 
 
 def main():
-    # default import
     default = "from adafruit_hid.consumer_control_code import ConsumerControlCode # type: ignore\nfrom adafruit_hid.keycode import Keycode # type: ignore\nfrom micropython import const # type: ignore\n\n"
+    matrixInput = [
+        [0, "PLAY_PAUSE"],
+        [1, "MUTE"],
+        [2, "CONTROL", "ALT", "SHIFT", "PAGE_UP"],
+        [3, "CONTROL", "ALT", "SHIFT", "PAGE_DOWN"],
+    ]
+    # [["port index", "true", "false"]]
+    analogInput = [[10, "VOLUME_INCREMENT", "VOLUME_DECREMENT"]]
+
     with open("app.py", "w") as f:
         # write into the file
         f.write(default)
 
-        # reformating a string and then creating a file
         nl = "\n"
         lambdaKdb = ": lambda cc, kbd:"
+        lambdaCc = "lambda cc:"
+        cc = "cc.send("
+        ccCode = "ConsumerControlCode."
+        kdb = "kdb.send("
+        kCode = "Keycode."
         content = ""
 
-        # array of contents of the Matrix function
-        matrixArr = [
-            "MATRIX_ACTIONS = {",
-            nl,
-            0,
-            lambdaKdb,
-            " cc.send(ConsumerControlCode.PLAY_PAUSE),",
-            nl,
-            1,
-            lambdaKdb,
-            " cc.send(ConsumerControlCode.MUTE),",
-            nl,
-            2,
-            lambdaKdb,
-            "cc.send(ConsumerControlCode.SCAN_NEXT_TRACK),",
-            nl,
-            3,
-            lambdaKdb,
-            "kbd.send(Keycode.CONTROL, Keycode.ALT, Keycode.SHIFT, Keycode.PAGE_DOWN),",
-            nl,
-            "}",
-        ]
+        # create a matrix actions
+        matrixArr = []
+        matrixArr.append("MATRIX_ACTIONS = {\n")
+        for x in matrixInput:
+            matrixArr.append(x[0])
+            if len(x) == 2:
+                matrixArr.append((lambdaKdb + cc + ccCode + x[1] + "),"))
+            else:
+                matrixArr.append((lambdaKdb + kdb))
+                for i in range(len(x) - 1):
+                    matrixArr.append((kCode + x[i + 1]))
+                    if i < len(x) - 2:
+                        matrixArr.append(",")
+                    else:
+                        matrixArr.append("),")
+            matrixArr.append(nl)
+
+        matrixArr.append("}\n\n")
+
+        # import the content of the array to a one "content" string
         for i in matrixArr:
+            content += str(i)
+
+        # create analog actions contents
+        analogArr = []
+        analogArr.append("ANALOG_ACTIONS = {\n")
+        for x in analogInput:
+            analogArr.append((str(x[0]) + ": {" + nl))
+            analogArr.append(("True:" + lambdaCc + cc + ccCode + x[1] + "),\n"))
+            analogArr.append(("False:" + lambdaCc + cc + ccCode + x[2] + "),\n"))
+            analogArr += "},\n"
+        analogArr += "}"
+
+        # import the content of the array to a one "content" string
+        for i in analogArr:
             content += str(i)
 
         # rewrite the content of the file to python syntax using 'autopep8'
