2a3059ae878666b1dca7e645461730e25721a4d9
kubajznicek <kubajznicek0@gmail.com>
2024-01-08 21:10:06 +0100

    system status logging functions, ssd1306 display (libs + text render + bitmap render)

--- firmware/functions.py
+++ firmware/functions.py
@@ -1,10 +1,12 @@
+import gc
+import os
 import microcontroller # type: ignore
 import json
 
-def set_digital_pin(pin: digitalio.DigitalInOut, value: bool):
+def set_digital_pin(pin: digitalio.DigitalInOut, value: bool) -> None:
     pin.value = value
 
-def log_cpu_info():
+def log_cpu_info() -> None:
     """
     Logs CPU information to the console.
 
@@ -45,4 +47,30 @@ def read_matrix_config(file_name: str) -> dict:
 
 
     return config
+
+def print_storage_info():
+    """
+    Prints storage information to the console.
+    """
+    s = os.statvfs('/')
+    total_space = s[0] * s[2]
+    free_space = s[0] * s[3]
+    used_space = total_space - free_space
+
+    print('Total storage: ', total_space)
+    print('Used storage: ', used_space)
+    print('Free storage: ', free_space)
     
+
+def print_ram_info():
+    """
+    Prints RAM information to the console.
+    """
+    gc.collect()  # run the garbage collector to free up as much RAM as possible
+    total_ram = gc.mem_alloc() + gc.mem_free()
+    used_ram = gc.mem_alloc()
+    free_ram = gc.mem_free()
+
+    print('Total RAM: ', total_ram)
+    print('Used RAM: ', used_ram)
+    print('Free RAM: ', free_ram)
